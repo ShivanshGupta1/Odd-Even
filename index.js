59d6b1d6db6a381a95023c9cee5f267712bf39b8
Shivansh Gupta <mail@shivanshgupta.com>
2020-12-02 09:42:22 +0530

    Project Completed

---
+++ index.js
@@ -0,0 +1,12 @@
+function check() {
+ 
+    let a= document.getElementById("a").value;
+ 
+a = parseInt(a)
+
+    if (a%2==0)
+    {
+       alert('The number is even')
+    }
+    else {alert('The number is odd')}
+}
